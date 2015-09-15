fe8eed05eec1ec1ff5a223c096117b22d35de99e
nemtiax <nemtiax@gmail.com>
2015-09-15 16:29:37 -0400

    Revert "Revert "added grey circles at Nodes, added grey roads with dashes. Still needs lots of work""
    This reverts commit f72b26446c6b87130609b37b234dd5a47e6f4f5a.

--- Traffic/src/frontend/Front.java
+++ Traffic/src/frontend/Front.java
@@ -1,10 +1,12 @@
 package frontend;
 
+import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
@@ -22,8 +24,11 @@ import GraphModel.GraphGenerator;
 import GraphModel.Node;
 
 public class Front extends JFrame {
+	final static float dash1[] = {5.0f};
+	final static BasicStroke dashed = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, dash1, 0.0f);
 	Graph graph;
 	private final int ARR_SIZE = 4;
+
 	public Front(Graph g) {
 		this.graph = g;
 		initUI();
@@ -33,9 +38,6 @@ public class Front extends JFrame {
 		// Create and set up the window.
 		JFrame frame = new JFrame("Amazing Interface");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		JLabel emptyLabel = new JLabel("Hello");
-		frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
 
 		int maxX = 0;
 		int maxY = 0;
@@ -53,6 +55,7 @@ public class Front extends JFrame {
 		Set<Edge> allEdges = graph.getAllEdges();
 		maxX += 20;
 		maxY /= 2;
+		// maxX /= 2;
 		maxY += 20;
 		BufferedImage image = createImage(allEdges, allNodes, maxX, maxY);
 
@@ -68,53 +71,56 @@ public class Front extends JFrame {
 
 		BufferedImage image = new BufferedImage(x, y, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2 = (Graphics2D) image.getGraphics();
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2.setColor(Color.BLACK);
 		// g2.drawString("Hello world", 30, 30);
 
-		for (Node n : allNodes) {
-			Point2D pos = n.getPosition();
-			addNode(n, g2);
-		}
-		for (Edge e: allEdges)
-		{
-			Point2D startPos = e.getStart().getPosition();
-			Point2D endPos = e.getEnd().getPosition();
-			//drawArrow(g2, (int) startPos.getX(),(int)startPos.getY()/2,100,100);
-			//drawArrow(g2, 30,300,300,190);
-			drawArrow(g2,e);
-		}
+		drawEdges(allEdges, g2);
+		drawNodes(allNodes, g2);
 		return image;
 	}
-//Luka version that works, but undirected arrows
-	private void drawArrow(Graphics2D g2, Edge e) {
+
+	private void drawNodes(Set<Node> allNodes, Graphics2D g2) {
+
+		for (Node n : allNodes) {
+			// Point2D pos = n.getPosition();
+			addNode(n, g2);
+		}
+	}
+
+	private void drawEdges(Set<Edge> allEdges, Graphics2D g2) {
+		for (Edge e : allEdges) {
+			// drawArrow(g2, (int)
+			// startPos.getX(),(int)startPos.getY()/2,100,100);
+			// drawArrow(g2, 30,300,300,190);
+			drawRoad(g2, e);
+		}
+	}
+
+	// Luka version that works, but undirected arrows
+	private void drawRoad(Graphics2D g2, Edge e) {
 		Point2D startPos = e.getStart().getPosition();
 		Point2D endPos = e.getEnd().getPosition();
-		g2.drawLine((int)startPos.getX(), (int)startPos.getY() / 2, (int)endPos.getX(), (int)endPos.getY() / 2);
-	
+		g2.setStroke(new BasicStroke(10));
+		g2.setColor(Color.gray);
+		g2.drawLine((int) startPos.getX(), (int) startPos.getY() / 2, (int) endPos.getX(), (int) endPos.getY() / 2);
+		g2.setColor(Color.white);
+		g2.setStroke(dashed);
+		g2.drawLine((int) startPos.getX(),(int) startPos.getY() / 2 , (int) endPos.getX(), (int) endPos.getY() / 2);
+		g2.setStroke(new BasicStroke(1));
+		g2.setColor(Color.black);
+
 	}
-	//stackoverflow version that almost works
-	  void drawArrow(Graphics2D g, int x1, int y1, int x2, int y2) {
-         // Graphics2D g = (Graphics2D) g1.create();
-
-          double dx = x2 - x1, dy = y2 - y1;
-          double angle = Math.atan2(dy, dx);
-          int len = (int) Math.sqrt(dx*dx + dy*dy);
-          AffineTransform at = AffineTransform.getTranslateInstance(x1, y1);
-          at.concatenate(AffineTransform.getRotateInstance(angle));
-          g.transform(at);
-
-          // Draw horizontal arrow starting in (0, 0)
-          g.drawLine(0, 0, len, 0);
-          g.fillPolygon(new int[] {len, len-ARR_SIZE, len-ARR_SIZE, len},
-                        new int[] {0, -ARR_SIZE, ARR_SIZE, 0}, 4);
-      }
 
 	private void addNode(Node n, Graphics2D graphic) {
-		graphic.setColor(Color.RED);
-		//graphic.drawRect((int)n.getPosition().getX(), (int)n.getPosition().getY(), 10, 10);
-		graphic.fillRect((int)n.getPosition().getX(), (int)n.getPosition().getY()/2, 10, 10);
+		graphic.setColor(Color.GRAY);
+		// graphic.drawRect((int)n.getPosition().getX(),
+		// (int)n.getPosition().getY(), 10, 10);
+		// graphic.fillRect((int)n.getPosition().getX(),
+		// (int)n.getPosition().getY()/2, 10, 10);
+		graphic.fillOval((int) n.getPosition().getX() - 10, (int) n.getPosition().getY() / 2 - 10, 20, 20);
 		graphic.setColor(Color.BLACK);
-		graphic.drawString(n.getLabel(),(int)n.getPosition().getX(), (int)n.getPosition().getY()/2);
+		graphic.drawString(n.getLabel(), (int) n.getPosition().getX(), (int) n.getPosition().getY() / 2);
 
 	}
 
@@ -143,17 +149,14 @@ public class Front extends JFrame {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				GraphGenerator gen = new GraphGenerator(10, 10);
-				
-				
-				
 				Graph g = null;
 				try {
-					g = Graph.readFromFile(new File("Graphs/SimpleDiamond.txt"),false);
+					g = Graph.readFromFile(new File("Graphs/SimpleDiamond.txt"), false);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				//g.generateShortestPaths();
+				// g.generateShortestPaths();
 				Front f = new Front(g);
 				g.toString();
 
